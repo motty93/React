3be192b5d182d14ad58a3b61596871f051de1830
motty93 <rdwbocungelt5@gmail.com>
2019-10-03 10:23:28 +0900

    update: 仮画面

--- next-micro-post/pages/index.js
+++ next-micro-post/pages/index.js
@@ -1,3 +1,25 @@
 import React from 'react'
 
-export default () => <h1>Hello World</h1>
+// export default () => <h1>Hello World</h1>
+export default class extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '80vh'
+        }}
+      >
+        <h1>Sign Up</h1>
+        <div>Email</div>
+        <input type="text" />
+        <div>Password</div>
+        <input type="password" />
+        <button>submit</button>
+      </div>
+    )
+  }
+}
