3d464fa4378d3ed3590d73385b793cbad2eb16bf
motty93 <rdwbocungelt5@gmail.com>
2019-10-10 11:30:10 +0900

    update: state name onchange

--- react_base/src/App.js
+++ react_base/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    name: '',
+  }
+
+  onChangeText = e => {
+    this.setState({ name: e.target.value })
+  }
+
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <input type="text" onChange={this.onChangeText} />
+      </div>
+    )
   }
 }
 
