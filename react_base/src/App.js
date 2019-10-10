608682b92a9171e96c9351b57371ca91199b8fe9
motty93 <rdwbocungelt5@gmail.com>
2019-10-10 11:44:50 +0900

    add: username component

--- react_base/src/App.js
+++ react_base/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import UserName from './username'
 
 class App extends React.Component {
   state = {
@@ -12,7 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.state.name}</h1>
+        <UserName name={this.state.name} />
         <input type="text" onChange={this.onChangeText} />
       </div>
     )
