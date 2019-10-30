a31e39eb855df7d8432281ff6f907f778032a0fc
motty93 <rdwbocungelt5@gmail.com>
2019-10-30 22:23:40 +0900

    add: LikeButton Component

---
+++ good-button/src/LikeButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class LikeButton extends React.Component {
+  render() {
+    return (
+      <span>いいねボタン予定地</span>
+    );
+  }
+}
+
+export default LikeButton;
