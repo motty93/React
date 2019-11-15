7a4fad5bd8d019195b05b9bf28769adbd96058d5
motty93 <rdwbocungelt5@gmail.com>
2019-11-15 18:01:10 +0900

    add: state & onClickButton

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import { Person } from './Person/Person';
 
 class App extends React.Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 29 },
+      { name: 'manu', age: 22 },
+      { name: 'john', age: 20 },
+    ]
+  }
+
+  onClickSwitchButton = () => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: this.state.persons[0].age + 1 },
+        { name: 'manu', age: this.state.persons[1].age + 1 },
+        { name: 'john', age: this.state.persons[2].age + 1 },
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person name="Max" age="29" />
-        <Person name="manu" age="22">My Hobbieds: training</Person>
-        <Person name="john" age="20" />
+        <button
+          onClick={this.onClickSwitchButton}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: training</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Him I\'m a React App');
