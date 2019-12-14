0fc17c0fdaadee11040ee1bce0ff39e40c31961f
motty93 <rdwbocungelt5@gmail.com>
2019-12-14 10:45:00 +0900

    update: prettier

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,47 +1,55 @@
-import React, { useState } from 'react';
-import './App.css';
-import { Person } from './Person/Person';
+import React, {useState} from 'react'
+import './App.css'
+import {Person} from './Person/Person'
 
 const app = props => {
   const [personState, setPersonState] = useState({
     persons: [
-      { name: 'Max', age: 29 },
-      { name: 'manu', age: 22 },
-      { name: 'john', age: 20 },
-    ]
+      {name: 'Max', age: 29},
+      {name: 'manu', age: 22},
+      {name: 'john', age: 20},
+    ],
   })
 
   // const [otherState, setOtherState] = useState('some other value')
-  const [showPersonsState, setShowPersonsState] = useState({ show: false })
+  const [showPersonsState, setShowPersonsState] = useState({show: false})
 
-  const onClickSwitchButton = (newName) => {
-    setPersonState ({
+  const onClickSwitchButton = newName => {
+    setPersonState({
       persons: [
-        { name: newName, age: personState.persons[0].age + 1 },
-        { name: personState.persons[1].name, age: personState.persons[1].age + 1 },
-        { name: personState.persons[2].name, age: personState.persons[2].age + 1 },
-      ]
+        {name: newName, age: personState.persons[0].age + 1},
+        {
+          name: personState.persons[1].name,
+          age: personState.persons[1].age + 1,
+        },
+        {
+          name: personState.persons[2].name,
+          age: personState.persons[2].age + 1,
+        },
+      ],
     })
   }
 
-  const nameChangeHandler = (e) => {
-    setPersonState ({
+  const nameChangeHandler = e => {
+    setPersonState({
       persons: [
-        { name: personState.persons[0].name, age: personState.persons[0].age },
-        { name: e.target.value, age: personState.persons[1].age },
-        { name: personState.persons[2].name, age: personState.persons[2].age },
-      ]
+        {name: personState.persons[0].name, age: personState.persons[0].age},
+        {name: e.target.value, age: personState.persons[1].age},
+        {name: personState.persons[2].name, age: personState.persons[2].age},
+      ],
     })
   }
 
-  const deletePersonHandler = (personIndex) => {
-    let newPersons = personState.persons.filter((_person, index) => index != personIndex)
+  const deletePersonHandler = personIndex => {
+    let newPersons = personState.persons.filter(
+      (_person, index) => index != personIndex,
+    )
     setPersonState({persons: newPersons})
   }
 
   const togglePersonsHandler = () => {
     const doesShow = showPersonsState.show
-    setShowPersonsState({ show: !doesShow })
+    setShowPersonsState({show: !doesShow})
   }
 
   const style = {
@@ -49,7 +57,7 @@ const app = props => {
     font: 'inherit',
     border: '1px solid blue',
     padding: '8px',
-    cursor: 'pointer'
+    cursor: 'pointer',
   }
 
   let persons = null
@@ -57,10 +65,13 @@ const app = props => {
     persons = (
       <div>
         {personState.persons.map((person, index) => {
-          return <Person
-            click={() => deletePersonHandler(index)}
-            name={person.name}
-            age={person.age} />
+          return (
+            <Person
+              click={() => deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+            />
+          )
         })}
       </div>
     )
@@ -70,12 +81,12 @@ const app = props => {
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working</p>
-      <button
-        style={style}
-        onClick={togglePersonsHandler}>Toggle Persons</button>
+      <button style={style} onClick={togglePersonsHandler}>
+        Toggle Persons
+      </button>
       {persons}
     </div>
-  );
+  )
 }
 
-export default app;
+export default app
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from 'react'
 import './Person.css'
 
-export const Person = (props) => {
+export const Person = props => {
   return (
     <div className="Person">
-      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old.</p>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old.
+      </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
-    </div >
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
   )
 }
