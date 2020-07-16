50a9f9e78db6af5c4930ccfa26444fef7efa4edc
motty93 <rdwbocungelt5@gmail.com>
2020-07-16 22:03:07 +0900

    update: counter hook components

--- react-hooks/src/components/CounterHook.js
+++ react-hooks/src/components/CounterHook.js
@@ -3,19 +3,27 @@ import React, { useState } from 'react'
 // 関数コンポーネント
 // フックを呼び出すのはトップレベルのみ
 // フックを呼び出すのはReactの関数内のみ
-function CounterHook() {
+export const CounterHook = () => {
   const [count, setCount] = useState(0)
 
   const incrementCount = () => {
-    setCount(count + 1)
+    setCount(prevCount => prevCount + 1)
+  }
+
+  const incrementCountTen = () => {
+    for(let i = 0; i < 10; i++) {
+      // これだとうまくいかない
+      // setCount(count + 1)
+      // console.log(count)
+      setCount(prevCount => prevCount + 1)
+    }
   }
 
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={incrementCount}>カウント+</button>
+      <button onClick={incrementCountTen}>カウント10+</button>
     </div>
   )
 }
-
-export default CounterHook
