93785ed9499f73ef46fccf19751113d5b43a2c8a
motty93 <rdwbocungelt5@gmail.com>
2020-12-09 10:51:32 +0900

    add: useState

--- react-lesson/src/App.tsx
+++ react-lesson/src/App.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState }from 'react'
 import './App.css'
 import { Child } from './components/Child'
 
 const App: React.FC = () => {
+  const [count, setCount] = useState<number>(0)
+  const [message, setMessage] = useState<string>('')
+
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
+  const handleDecrement = () => {
+    setCount(count - 1)
+  }
+  const handleMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value)
+  }
+
   return (
     <div className="App">
       <Child message={'test'}>
         <p>子コンポーネントを渡す</p>
-     </Child>
+      </Child>
+      <p>{count}</p>
+      <p>
+        <button onClick={handleIncrement}>+1</button>
+        <button onClick={handleDecrement}>-1</button>
+      </p>
+      <p>{message}</p>
+      <input type="text" value={message} onChange={handleMessage}/>
     </div>
   )
 }
