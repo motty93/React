6b984846f0736f4e1b796d98994cb222719b130a
motty93 <rdwbocungelt5@gmail.com>
2020-12-10 12:39:43 +0900

    add: new task submit

--- react-todo/src/components/TaskInput.tsx
+++ react-todo/src/components/TaskInput.tsx
@@ -1,9 +1,30 @@
 import React, { useState } from 'react'
+import { Task } from '../Types'
 
-export const TaskInput: React.FC = () => {
+type Props = {
+  tasks: Task[]
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>
+}
+
+export const TaskInput: React.FC<Props> = ({ tasks, setTasks }) => {
   const [inputTitle, setInputTitle] = useState('')
+  const [count, setCount] = useState(tasks.length + 1)
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputTitle(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    setCount(count + 1)
+
+    const newTask: Task = {
+      id: count,
+      title: inputTitle,
+      done: false
+    }
+
+    setTasks([newTask, ...tasks])
+    setInputTitle('')
   }
 
   return (
@@ -15,7 +36,7 @@ export const TaskInput: React.FC = () => {
           value={inputTitle}
           onChange={handleInputChange}
         />
-        <button className="btn is-primary">追加</button>
+        <button className="btn is-primary" onClick={handleSubmit}>追加</button>
       </div>
     </div>
   )
