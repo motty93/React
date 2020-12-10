5801252f421e4dbaed2a87324c5ed49afd1ea94e
motty93 <rdwbocungelt5@gmail.com>
2020-12-10 18:05:50 +0900

    update: dispatch & action for task item

--- react-todo/src/components/TaskItem.tsx
+++ react-todo/src/components/TaskItem.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import { Task } from '../Types'
+import { doneTask, deleteTask } from '../modules/tasksModule'
+import { useDispatch } from 'react-redux'
 
 type Props = {
   task: Task
-  handleDone: (task: Task) => void
-  handleDelete: (task: Task) => void
 }
 
-export const TaskItem: React.FC<Props> = ({
-  task,
-  handleDone,
-  handleDelete
-}) => {
+export const TaskItem: React.FC<Props> = ({ task }) => {
+  const dispatch = useDispatch()
+
   return (
     <li className={task.done ? 'done' : ''}>
       <label>
         <input
           type="checkbox"
           className="checkbox-input"
-          onClick={() => handleDone(task)}
+          onClick={() => dispatch(doneTask(task))}
           defaultChecked={task.done}
         />
         <span className="checkbox-label">{task.title}</span>
-        <button onClick={() => handleDelete(task)} className="btn is-delete">
+        <button
+          onClick={() => dispatch(deleteTask(task))}
+          className="btn is-delete"
+        >
           削除
         </button>
       </label>
