d5d0e731fb831998dd1b808c4021cd47fa771922
motty93 <rdwbocungelt5@gmail.com>
2020-12-10 18:05:10 +0900

    update: useSelectorを使用

--- react-todo/src/components/TaskList.tsx
+++ react-todo/src/components/TaskList.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
 import { TaskItem } from './TaskItem'
-import { Task } from '../Types'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { useSelector } from 'react-redux'
+import { RootState } from '../rootReducer'
 
-type Props = {
-  tasks: Task[]
-  setTasks: React.Dispatch<React.SetStateAction<Task[]>>
-}
-
-export const TaskList: React.FC<Props> = ({ tasks, setTasks }) => {
-
-  const handleDone = (task: Task) => {
-    setTasks(prev =>
-      prev.map(t => (t.id === task.id ? { ...task, done: !task.done } : t))
-    )
-  }
-
-  const handleDelete = (task: Task) => {
-    setTasks(prev => prev.filter(t => t.id !== task.id))
-  }
-
+export const TaskList: React.FC = () => {
+  const { tasks } = useSelector((state: RootState) => state.tasks)
   return (
     <div className="inner">
       {tasks.length <= 0 ? (
@@ -28,19 +14,14 @@ export const TaskList: React.FC<Props> = ({ tasks, setTasks }) => {
         <TransitionGroup component="ul" className="task-list">
           {tasks.map(task => (
             <CSSTransition
-            key={task.id}
-            timeout={ {
-              enter: 300,
-                exit: 700
-            } }
-            classNames="fade"
-            >
-            <TaskItem
               key={task.id}
-              task={task}
-              handleDone={handleDone}
-              handleDelete={handleDelete}
-            />
+              timeout={{
+                enter: 300,
+                exit: 700
+              }}
+              classNames="fade"
+            >
+              <TaskItem key={task.id} task={task} />
             </CSSTransition>
           ))}
         </TransitionGroup>
