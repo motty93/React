a1c9a0fd8c25548587a005b0c37b176aec9e10bb
motty93 <rdwbocungelt5@gmail.com>
2020-12-10 17:21:17 +0900

    add: task module

---
+++ react-todo/src/modules/tasksModule.ts
@@ -0,0 +1,58 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Task } from '../Types'
+
+type State = {
+  count: number
+  tasks: Task[]
+}
+
+const initialState: State = {
+  count: 2,
+  tasks: [
+    {
+      id: 2,
+      title: 'つぎのtodo',
+      done: false
+    },
+    {
+      id: 1,
+      title: '最初のtodo',
+      done: true
+    }
+  ]
+}
+
+const tasksModule = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask(state: State, action: PayloadAction<string>) {
+      state.count++
+
+      const newTask: Task = {
+        id: state.count,
+        title: action.payload, // 引数で受け取るのでpayload
+        done: false
+      }
+
+      state.tasks = [newTask, ...state.tasks]
+    },
+    doneTask(state: State, action: PayloadAction<Task>) {
+      const task = state.tasks.find(t => t.id === action.payload.id)
+      if (task) {
+        task.done = !task.done
+      }
+      // state.tasks = state.tasks(prev =>
+      //   prev.map(t =>
+      //     t.id === action.payload.id ? { ...t, done: !action.payload.done } : t
+      //   )
+      // )
+    },
+    deleteTask(state: State, action: PayloadAction<Task>) {
+      state.tasks = state.tasks.filter(t => t.id !== action.payload.id)
+    }
+  }
+})
+
+export const { addTask, doneTask, deleteTask } = tasksModule.actions
+export default tasksModule
