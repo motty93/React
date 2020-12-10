bf0afab4f627eb5c171e2aafcbef2d1539591871
motty93 <rdwbocungelt5@gmail.com>
2020-12-10 11:51:06 +0900

    add: styled-components

--- react-lesson/src/App.tsx
+++ react-lesson/src/App.tsx
@@ -1,10 +1,16 @@
-import React, { useState }from 'react'
+import React, { useState } from 'react'
+import styled, { css } from 'styled-components'
 import './App.css'
 import { Child } from './components/Child'
+
+type TitleProps = {
+  blue?: boolean
+}
 
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0)
   const [message, setMessage] = useState<string>('')
+  const [blue, setBlue] = useState(false)
 
   const handleIncrement = () => {
     setCount(count + 1)
@@ -15,9 +21,15 @@ const App: React.FC = () => {
   const handleMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value)
   }
+  const changeBlueColorHandler = () => {
+    setBlue(!blue)
+  }
 
   return (
     <div className="App">
+      <Title blue={blue} onClick={changeBlueColorHandler}>
+        React スタイル設定
+      </Title>
       <Child message={'test'}>
         <p>子コンポーネントを渡す</p>
       </Child>
@@ -27,9 +39,20 @@ const App: React.FC = () => {
         <button onClick={handleDecrement}>-1</button>
       </p>
       <p>{message}</p>
-      <input type="text" value={message} onChange={handleMessage}/>
+      <input type="text" value={message} onChange={handleMessage} />
     </div>
   )
 }
 
 export default App
+
+const Title = styled.h1<TitleProps>`
+  color: red;
+
+  ${({ blue }) =>
+    blue
+      ? css`
+          color: blue;
+        `
+      : ``}
+`
