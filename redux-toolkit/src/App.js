cbab86c9185205b4054374ca2694a2794f989c30
motty93 <rdwbocungelt5@gmail.com>
2021-04-21 15:51:51 +0900

    feat: hello redux toolkit

---
+++ redux-toolkit/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Counter } from './features/counter/Counter'
+import './App.css'
+
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <Counter/>
+    </header>
+  </div>
+)
